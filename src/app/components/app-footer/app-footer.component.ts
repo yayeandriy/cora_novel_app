711245fa9473ba9a5c4c4f1fb780fe5ac6761d78
Andriy Yeroshevych <yayeandriy.com@gmail.com>
2025-12-30 13:16:32 +0200

    stat fix

--- src/app/components/app-footer/app-footer.component.ts
+++ src/app/components/app-footer/app-footer.component.ts
@@ -200,4 +200,8 @@ export class AppFooterComponent implements OnInit {
     if (num >= 1000) return Math.floor(num / 1000) + 'K';
     return num.toString();
   }
+
+  formatNumberFull(num: number): string {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '\u202F');
+  }
 }
